21b84ba6ec11deea1d141d57f009a4d73698fe13
MartyMacFly <fred_kun@hotmail.fr>
2014-09-03 17:53:14 +0200

    Add images

--- js/cv.js
+++ js/cv.js
@@ -1,6 +1,9 @@
 $(function(){
 
-	$("li").click(function(){
+	$("#divMenu li").click(function(){
+		$("#divMenu li").removeClass("menuSelected");
+		$(this).addClass("menuSelected");
+		
 		$(".content").css("display", "none");
 		var idContent = "#" + $(this).attr("id") + "Content";
 		$(idContent).css("display", "block");
