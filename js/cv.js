fa7fa5a74231a7d1ad2e715ae270fb384d31d8c7
MartyMacFly <fred_kun@hotmail.fr>
2014-09-04 14:24:55 +0200

    Add animations

--- js/cv.js
+++ js/cv.js
@@ -3,9 +3,22 @@ $(function(){
 	$("#divMenu li").click(function(){
 		$("#divMenu li").removeClass("menuSelected");
 		$(this).addClass("menuSelected");
-		
-		$(".content").css("display", "none");
+
 		var idContent = "#" + $(this).attr("id") + "Content";
-		$(idContent).css("display", "block");
+		$(".contentSelected").one('transitionend', function(e)
+		{
+			$(idContent).addClass("contentSelected");
+			//$(contentSelected).addClass("contentSelected");
+		});
+		$(".contentSelected").removeClass("contentSelected");
+		
+		
+		
+		
+		
+		/*$(idContent).one('transitionend', function(e)
+		{
+			
+		});*/
 	});
 });
